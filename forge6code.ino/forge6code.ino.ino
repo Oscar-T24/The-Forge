d796cb8dbdd08c8f335453fc5144570e187a16db
Oscar-T24 <o.tesniere24@ejm.org>
2025-02-08 11:24:35 -0500

    update

--- forge6code.ino/forge6code.ino.ino
+++ forge6code.ino/forge6code.ino.ino
@@ -1,16 +1,25 @@
 void setup() {
  
+ // Configuration : Broche ENA sur pin 3 (PWM) broche IN1 sur pin 2
+ pinMode(3,OUTPUT);
+pinMode(2,OUTPUT);
  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+/*
   Serial.print(analogRead(A1));
   Serial.print(" and reading two ");
   Serial.print(analogRead(A2));
     Serial.print(" and reading three ");
      Serial.print(analogRead(A5));
   Serial.println();
+  */
+  digitalWrite(2,HIGH);
+  for(unsigned char i = 0; i<255;i++){
+    analogWrite(3,i);
+  }
 
 }
